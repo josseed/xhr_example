c797824c5dc514b9ce8fba4285311d68f69bd43c
Jose <jose.zunigahenriquez@gmail.com>
2022-05-15 21:55:38 -0400

    🚀 consume api and populate select

--- main.js
+++ main.js
@@ -1,24 +1,49 @@
 const xhr = new XMLHttpRequest();
-        
+
 const contact = document.getElementById('contact');
 contact.addEventListener('click', () => {
-    changeSite('form.html');
+  changeSite('form.html');
+  getRegions();
 });
 
 const aboutUs = document.getElementById('aboutUs');
 aboutUs.addEventListener('click', () => {
-    changeSite('perro.html');
+  changeSite('perro.html');
 });
 
 function changeSite(url) {
-    const container = document.getElementById('container');
-    xhr.onload = function() {
-        if (this.status === 200) {
-            container.innerHTML = this.responseText;
-        } else {
-            console.warn('did not receive 200 OK from server');
-        }
-    };
-    xhr.open('get', url);
-    xhr.send();
+  const container = document.getElementById('container');
+  xhr.onload = function () {
+    if (this.status === 200) {
+      container.innerHTML = this.responseText;
+    } else {
+      console.warn('did not receive 200 OK from server');
+    }
+  };
+  xhr.open('get', url);
+  xhr.send();
+}
+
+function getRegions() {
+  const _xhr = new XMLHttpRequest();
+  _xhr.onload = function () {
+    if (this.status === 200) {
+      regions = JSON.parse(this.responseText);
+      console.log(regions);
+      select = document.getElementById('selectRegionId');
+      while (select == null) {
+        select = document.getElementById('selectRegionId');
+        console.log('select is null');
+      }
+      for (let index = 0; index < regions.length; index++) {
+        const region = regions[index];
+        let optionElem = document.createElement('option');
+        optionElem.value = region.codigo;
+        optionElem.textContent = region.nombre;
+        select.appendChild(optionElem);
+      }   
+    }
+  }
+  _xhr.open('get', 'https://apis.digital.gob.cl/dpa/regiones');
+  _xhr.send();
 }
